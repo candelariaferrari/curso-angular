4313ae4c09ffbe6024a269ec980cdcdd914d37d9
Candelaria <ferrari.candelaria@gmail.com>
2025-08-20 14:50:52 -0300

    rutas y rutas hijas

--- 03-gifs-app/src/app/app.routes.ts
+++ 03-gifs-app/src/app/app.routes.ts
@@ -8,15 +8,23 @@ export const routes: Routes = [
   //forma mas elegante
   {
     path: 'dashboard',
-    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component') //voy al componente
-  },
-  { //para mostrar un infinite scroll de todos los gifs que estan aca
-    path:'trending',
-    loadComponent: () => import('./gifs/pages/trending-page/trending-page.component')
-  },
-  {
-    path:'search',
-    loadComponent: () => import('./gifs/pages/search-page/search-page.component')
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'), //voy al componente
+    //RUTAS HIJAS , son arreglos de rutas
+    children:[
+      { //para mostrar un infinite scroll de todos los gifs que estan aca
+        path:'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page.component')
+      },
+      {
+        path:'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page.component')
+      },
+      { //aca a cualquier ruta que vaya me lleva al trending
+        path:'**',
+        redirectTo: 'trending'
+      }
+    ]
+
   },
   { //aca a cualquier ruta que vaya me lleva al dash
     path:'**',
