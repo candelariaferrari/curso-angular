3c384f280278b3a2065b7081a0826188789d7f2e
Candelaria <ferrari.candelaria@gmail.com>
2025-08-20 12:17:02 -0300

    despligue y hashrouter

--- 02-bases/src/app/app.config.ts
+++ 02-bases/src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    //HashStrategy
+    {
+      provide:LocationStrategy,
+      useClass: HashLocationStrategy //hace que aparezca un # en el directorio
+      // funciona como un indicador que no hace que el nav web redireccione a esa carpeta
+      // estamos siempre en el root pero añade ese # + tal pag y angular sabe que hacer
+    }
+  ]
 };
--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -2,13 +2,15 @@ import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 //un servicio es una clase comun y corriente pero trabaja como un singleton
 
-const loadFromLocalStorage = (): Character[] => {
+const loadFromLocalStorage = (): Character[] => { //lo que guardamos aca por mas que el navegador se vuelva a cargar, va quedar en el localstorage
   const characters = localStorage.getItem('characters');
+
   return characters ? JSON.parse(characters): []; //transformamos el string a un objeto
 
-  return []
-}
 
+}
+//en el localstorage se puede modificar desde la consola del navegador por eso tener en cuenta de solo 
+// guardar informacion que no sea sensible 
 
 
 // la info siembre va a quedar si pasa por el servicio, no se va a destruir
@@ -17,11 +19,12 @@ const loadFromLocalStorage = (): Character[] => {
 })
 export class DragonballService {
   //arreglos
-  characters = signal<Character[]>([
+
+ /*  characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
-
+  ]); */
+characters = signal<Character[]>(loadFromLocalStorage()); //cambiamos el arreglo por el load
 // efectos es una funcion , que disparamos cada vez que queremos que algo suceda
 saveToLocalStorage = effect(()=>{
   localStorage.setItem('characters', JSON.stringify(this.characters())) //JSON.stringify hace del arreglo un string
