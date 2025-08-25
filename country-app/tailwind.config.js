041652fb4782637b241a22af51c786a6001d826f
Candelaria <ferrari.candelaria@gmail.com>
2025-08-25 17:13:34 -0300

    tailwind

---
+++ country-app/tailwind.config.js
@@ -0,0 +1,17 @@
+const { default: themes } = require('daisyui/theme/object');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui:{
+    themes: ["light"]
+  }
+}
