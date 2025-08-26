fb1bb66a178dd60dd9ebd5c5712993e8e489b646
Candelaria <ferrari.candelaria@gmail.com>
2025-08-26 11:01:23 -0300

    Footer

--- country-app/src/app/shared/pages/home-page/home-page.component.ts
+++ country-app/src/app/shared/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../../../country/components/footer/footer.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent { }
