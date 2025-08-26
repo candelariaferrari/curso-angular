76ccf26ad458e032d661665df9aa768b15f1951c
Candelaria <ferrari.candelaria@gmail.com>
2025-08-26 15:39:42 -0300

    traer info de postman

--- country-app/src/app/country/pages/by-capital/by-capital.component.ts
+++ country-app/src/app/country/pages/by-capital/by-capital.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
+import { RESTCountry } from '../../interfaces/rest-country.interfaces';
 
 @Component({
   selector: 'app-by-capital',
@@ -12,12 +13,22 @@ import { CountryService } from '../../services/country.service';
 export class ByCapitalComponent {
   countryService = inject(CountryService); //servicio
 
-  onSearch(query: string) { // el query de buscqueda
-    console.log(query);
-    this.countryService.searchByCapital(query) //disparar la peticion
-      .subscribe( resp => {
-        console.log(resp);
+  isLoading = signal(false);
+  isError = signal<string|null>(null);
+  countries = signal<RESTCountry[]>([])
 
+  onSearch(query: string) { // el query de buscqueda
+    if (this.isLoading()) return; // si esta en true que no haga nada
+
+    this.isLoading.set(true)
+    this.isError.set(null);
+
+    //console.log(query);
+    this.countryService.searchByCapital(query) //disparar la peticion
+      .subscribe( countries => {
+        //console.log(resp);
+        this.isLoading.set(false)
+        this.countries.set(countries)
       })
   }
 
