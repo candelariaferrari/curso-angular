4650ad21f29877dbf0bc08a2bcd583224f51c5d9
Candelaria <ferrari.candelaria@gmail.com>
2025-08-27 15:47:19 -0300

    pagina de region

--- country-app/src/app/country/components/search-input/search-input.component.ts
+++ country-app/src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, linkedSignal, output } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -9,5 +9,24 @@ export class SearchInputComponent {
 
   placeholder = input('Buscar');
   value = output<string>(); //recomienda no poner on a la hora de los eventos
+
+  debounceTime = input(1000); //tiempo que espera
+  initialValue = input<string>();///valor actualizado de la caja de texto, la persona va a tener siempre el ultimo valor que emitio
+
+
+  inputValue = linkedSignal<string>(() => this.initialValue() ?? '');
+
+  debounceEffect = effect((onCleanup) => { //Efecto , cada vez que disparamos el inputvalue disparamos el efecto
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => { //
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => { //limpiamos el efecto, ej: cuando destruimos el componente, cada vez que tenemos un nuevo valor
+      clearTimeout(timeout);
+    });
+  });
+
 
  }
--- country-app/src/app/country/services/country.service.ts
+++ country-app/src/app/country/services/country.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-country.interfaces';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of, tap } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
 import { Country } from '../interfaces/country.interfaces';
+import { Region } from '../interfaces/region.type';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -15,50 +16,92 @@ export class CountryService {
 
   //peticion http
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>(); //lo inicializamos como un map
+  private queryCacheCountry = new Map<string, Country[]>();
+  private queryCacheRegion = new Map<Region, Country[]>();
 
   //servicio
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
+
+    //Cache
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query) ?? []);
+    }
+
+
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-    .pipe(
-      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp) ),
-      //para atrapar el error
-      catchError(error => {
-        console.log('Error fetching ', error);
-        //lanzar error
-        return throwError(()=> new Error(`No se pudo obtener paises con ese query ${query}  `))
-      })
-    )
+      .pipe(
+        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+        tap((countries) => this.queryCacheCapital.set(query, countries)),
+        //para atrapar el error
+        catchError(error => {
+          console.log('Error fetching ', error);
+          //lanzar error
+          return throwError(() => new Error(`No se pudo obtener paises con ese query ${query}  `))
+        })
+      )
   }
+
+
 
   searchByCountry(query: string): Observable<Country[]> {
-    const url= `${API_URL}/name/${query}`;
+    const url = `${API_URL}/name/${query}`;
     query = query.toLowerCase();
-    return this.http.get<RESTCountry[]>(url)
-    .pipe(
-      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp) ),
-      delay(2000),
-      //para atrapar el error
-      catchError(error => {
-        console.log('Error fetching ', error);
-        //lanzar error
-        return throwError(()=> new Error(`No se pudo obtener paises con ese query ${query}  `))
-      })
-    )
-  }
-  searchCountryByAlphaCode(code: string){
-    const url= `${API_URL}/alpha/${code}`;
+
+    if (this.queryCacheCountry.has(query)) {
+      return of(this.queryCacheCountry.get(query) ?? []); //el of regresa un observable
+    }
 
     return this.http.get<RESTCountry[]>(url)
-    .pipe(
-      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp) ),
-      map(countries => countries.at(0)),
-      //para atrapar el error
-      catchError(error => {
+      .pipe(
+        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+        tap((countries) => this.queryCacheCountry.set(query, countries)),
+        delay(2000),
+        //para atrapar el error
+        catchError(error => {
+          console.log('Error fetching ', error);
+          //lanzar error
+          return throwError(() => new Error(`No se pudo obtener paises con ese query ${query}  `))
+        })
+      )
+  }
+
+  searchCountryByAlphaCode(code: string) {
+    const url = `${API_URL}/alpha/${code}`;
+
+    return this.http.get<RESTCountry[]>(url)
+      .pipe(
+        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+        map(countries => countries.at(0)),
+        //para atrapar el error
+        catchError(error => {
+          console.log('Error fetching ', error);
+          //lanzar error
+          return throwError(() => new Error(`No se pudo obtener paises con ese codigo ${code}  `))
+        })
+      )
+  }
+
+
+  searchByRegion(region: Region) {
+    const url = `${API_URL}/region/${region}`;
+
+
+    if (this.queryCacheCountry.has(region)) {
+      return of(this.queryCacheCountry.get(region) ?? []);
+    }
+
+    return this.http.get<RESTCountry[]>(url).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap((countries) => this.queryCacheRegion.set(region, countries)),
+      catchError((error) => {
         console.log('Error fetching ', error);
-        //lanzar error
-        return throwError(()=> new Error(`No se pudo obtener paises con ese codigo ${code}  `))
+
+        return throwError(
+          () => new Error(`No se pudo obtener países con ese query ${region}`)
+        );
       })
-    )
+    );
   }
 }
