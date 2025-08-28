24c21ef0c3da8531786aafa71eecc9ed68f0e4c8
Candelaria <ferrari.candelaria@gmail.com>
2025-08-28 10:53:40 -0300

    number

--- 05-pipes-app/src/app/pages/number-page/number-page.component.ts
+++ 05-pipes-app/src/app/pages/number-page/number-page.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './number-page.component.html',
 })
-export default class NumberPageComponent { }
+export default class NumberPageComponent {
+  totalSells = signal(2_433_232.5567); //podemos separar los miles con el _ 2_433_
+  percent = signal(0.4856);
+ }
+
