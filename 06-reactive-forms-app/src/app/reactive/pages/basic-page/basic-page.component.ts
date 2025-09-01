d7d06080304f4bb4c62f89521a94411253b355b5
Candelaria <ferrari.candelaria@gmail.com>
2025-09-01 11:44:28 -0300

    Form reactive

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,31 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+//import { FormUtils } from '../../../utils/form-utils';
+
 
 @Component({
   selector: 'app-basic-page',
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+
+//la aparecia, validaciones de mi form
+ myForm = new FormGroup({
+    name: new FormControl('', [], []),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  });
+
+
+}
